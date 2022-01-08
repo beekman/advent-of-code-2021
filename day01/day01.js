e05429f521aefe1ba44370741467ca380688973b
Ben Beekman <ben@riotnewmedia.com>
2022-01-07 18:19:04 -0800

    day 1 part 2 solution

--- day01/day01.js
+++ day01/day01.js
@@ -44,21 +44,77 @@ How many measurements are larger than the previous measurement?
 */
 
 const fs = require('fs');
-fs.readFile('./input.txt', 'utf8', function (err, data) {
+fs.readFile('./input.txt', 'utf8', function(err, data) {
   if(err) throw err;
 
   const numberOfDepthIncreases = data => {
 
-    let previousLine=0;
+    let previousLine = 0;
     const lines = data.split('\n');
     let timesDepthIncreased = 0;
     lines.forEach(line => {
-      if ( line > previousLine ) {
+      if(line > previousLine) {
         timesDepthIncreased++;
-    }
+      }
       previousLine = line;
     });
     console.log(timesDepthIncreased);
-  }
+  };
   numberOfDepthIncreases(data);
+
+  /* Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+  Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+  199  A
+  200  A B
+  208  A B C
+  210    B C D
+  200  E   C D
+  207  E F   D
+  240  E F G
+  269    F G H
+  260      G H
+  263        H
+  Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+  Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+  In the above example, the sum of each three-measurement window is as follows:
+
+  A: 607 (N/A - no previous sum)
+  B: 618 (increased)
+  C: 618 (no change)
+  D: 617 (decreased)
+  E: 647 (increased)
+  F: 716 (increased)
+  G: 769 (increased)
+  H: 792 (increased)
+  In this example, there are 5 sums that are larger than the previous sum.
+
+  Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+  */
+
+  const numberOfSlidingWindowSumIncreases = data => {
+    const lines = data.split('\n');
+    let previousSum = 0;
+    let timesSumIncreased = 0;
+    let currentIndex = 0;
+    lines.forEach(line => {
+      let currentSum = 0;
+
+      for(let i = 0; i < 3; i++) {
+        currentSum += parseInt(lines[i+currentIndex]);
+      }
+      if(currentSum > previousSum) {
+        timesSumIncreased++;
+
+      }
+      previousSum = currentSum;
+      currentIndex++;
+    });
+    console.log('Times Sum Increased', timesSumIncreased);
+  };
+  numberOfSlidingWindowSumIncreases(data);
 });
